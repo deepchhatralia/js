f8e063de7078197e7677c5c62a7a38903ef28ecb
deepchhatralia <84608736+deepchhatralia@users.noreply.github.com>
2021-09-06 16:03:24 +0530

    Update game.js

--- card-game/game.js
+++ card-game/game.js
@@ -14,7 +14,7 @@ let second = 0
 let clicked = 0
 
 setIcons()
-timer.innerHTML = '0' + minute + ' : ' + '10' + second
+timer.innerHTML = '0' + minute + ' : ' + '0' + second
 var timerr = setInterval(startTimer, 1000)
 
 
@@ -62,7 +62,7 @@ for (let i = 0; i < flipBoxInner.length; i++) {
                                 flipBoxInner[arr[j]].classList.remove('rotate-class')
                             }
                             arr = []
-                        }, 500);
+                        }, 100);
                     }
                 }
             }
